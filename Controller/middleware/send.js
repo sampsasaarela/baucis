3d193758533676b5042260d91c579917eda80bcf
William Riley-Land <william@kun.io>
2014-03-24 18:11:22 -0500

    First pass at reworking send middleware to use HighlandJS.

--- Controller/middleware/send.js
+++ Controller/middleware/send.js
@@ -1,76 +1,22 @@
 // __Dependencies__
+var _ = require('highland');
 var crypto = require('crypto');
 var errors = require('../../errors');
 
 // __Private Module Members__
-function singleOrArray () {
-  var first = false;
-  var multiple = false;
-
-  return es.through(
-    function (doc) {
-      if (!first) {
-        first = JSON.stringify(doc);
-      }
-      else if (!multiple) {
-        multiple = true;
-        this.emit('data', '[');
-        this.emit('data', first);
-        this.emit('data', ',\n')
-        this.emit('data', JSON.stringify(doc));
-      }
-      else {
-        this.emit('data', ',\n');
-        this.emit('data', JSON.stringify(doc));
-      }
-    },
-    function () {
-      if (!first) return this.emit('end');
-      else if (!multiple) this.emit('data', first);
-      else this.emit('data', ']');
-      this.emit('end');
-    }
-  );
-};
-
 function removeDocuments () {
   return es.map(function (doc, callback) {
     doc.remove(callback);
   });
 }
 
-function count () {
-  var count = 0;
-
-  return es.through(
-    function () { count += 1 },
-    function () {
-      this.emit('data', String(count));
-      this.emit('end');
-    }
-  );
-}
-
-function check404 () {
-  var count = 0;
-
-  return es.through(
-    function (doc) { count += 1, this.emit('data', doc) },
-    function () {
-      if (count === 0) return this.emit('error', errors.NotFound());
-      this.emit('end');
-    }
-  );
-}
-
-function etag (response) {
+function etag (a, b) {
   var hash = crypto.createHash('md5');
-  return es.map(function (doc, callback) {
-    if (response.get('Etag')) return callback(null, doc);
-    hash.update(JSON.stringify(doc));
-    response.set('Etag', '"' + hash.digest('hex') + '"');
-    callback(null, doc);
-  });
+  var etag = response.get('Etag');
+  if (etag) return etag
+  hash.update(JSON.stringify(doc));
+  response.set('Etag', '"' + hash.digest('hex') + '"');
+  callback(null, doc);
 }
 
 function lastModified (response, lastModifiedPath) {
@@ -81,68 +27,66 @@ function lastModified (response, lastModifiedPath) {
   });
 }
 
-function emptyString () {
-  return es.map(function (doc, callback) {
-    callback(null, '');
-  });
-}
-
 // __Module Definition__
 var decorator = module.exports = function (options, protect) {
   var controller = this;
 
+  // Create the basic stream.
   protect.finalize(function (request, response, next) {
     response.type('json');
-    request.baucis.pipe(check404(next));
+    request.baucis.send = _(request.baucis.documents).otherwise([ errors.NotFound() ])
+      .stopOnError(next).map(check404);
     next();
   });
 
-  protect.finalize('del', function (request, response, next) {
-    request.baucis.pipe(removeDocuments());
-    request.baucis.pipe(count());
-    next();
-  });
-
-  protect.finalize('put', function (request, response, next) {
-    request.baucis.pipe(JSONStream.stringify(false));
+  // HEAD
+  protect.finalize('instance', 'head', function (request, response, next) {
+    request.baucis.send.fork().map(lastModified).resume();
+    request.baucis.send.invoke('stringify', JSON);
+    request.baucis.send.fork().map(etag).resume();
+    request.baucis.send.reduce1(emptyString);
     next();
   });
 
   protect.finalize('collection', 'head', function (request, response, next) {
     // TODO use es.wait for setting etag and lastModified on collections?
-    request.baucis.pipe(emptyString());
-    next();
+    response.end();
   });
 
-  protect.finalize('instance', 'head', function (request, response, next) {
-    var lastModifiedPath = request.baucis.controller.get('lastModified');
-    request.baucis.pipe(etag(response));
-    request.baucis.pipe(lastModified(response, lastModifiedPath));
-    request.baucis.pipe(emptyString());
-    next();
-  });
-
+  // GET
   protect.finalize('instance', 'get', function (request, response, next) {
-    request.baucis.pipe(etag(response));
-    request.baucis.pipe(lastModified(response));
-    if (request.baucis.count) request.baucis.pipe(count());
-    else request.baucis.pipe(JSONStream.stringify(false));
+    request.baucis.send.fork().map(lastModified).resume();
+    request.baucis.send.invoke('stringify', JSON);
+    request.baucis.send.fork().map(etag).resume();
     next();
   });
 
   protect.finalize('collection', 'get', function (request, response, next) {
-    if (request.baucis.count) request.baucis.pipe(count());
-    else request.baucis.pipe(JSONStream.stringify());
+    request.baucis.send.invoke('stringify', JSON);
     next();
   });
 
+  // POST
   protect.finalize('collection', 'post', function (request, response, next) {
-    request.baucis.pipe(singleOrArray());
+    request.baucis.send.map(singleOrArray);
+    next();
+  });
+
+  // PUT
+  protect.finalize('put', function (request, response, next) {
+    request.baucis.send.invoke('stringify', JSON);
+    next();
+  });
+
+  protect.finalize('del', function (request, response, next) {
+    request.baucis.send.map(remove);
+    request.baucis.count = true;
     next();
   });
 
   protect.finalize(function (request, response, next) {
-    request.baucis.pipe();
+    if (request.baucis.count) request.baucis.send.reduce(count, 0).map(String);
+    request.baucis.send.pipe(response);
   });
 };
 
