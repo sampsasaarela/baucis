72c7f1c9c3bd14e3b5e3c81c9b586ba1cdade85e
William Riley-Land <william@kun.io>
2014-03-24 14:31:42 -0500

    Refactor streaming into two methods.

--- Controller/middleware/documents/stream.js
+++ Controller/middleware/documents/stream.js
@@ -2,17 +2,26 @@
 var es = require('event-stream');
 var JSONStream = require('JSONStream');
 
+// __Private Module Members__
+// Genereate a function that will add pipes to an array.  Then, when called
+// with no arguments,
+function pipeInterface (source, finalize) {
+  var pipes = [];
+  return function (destination) {
+    if (destination !== undefined) return pipes.push(destination);
+    pipes.unshift(source());
+    pipes.push(finalize);
+    var stream = es.pipeline.apply(es, pipes);
+    stream.on('error', next);
+  };
+}
+
 // __Module Definition__
 var decorator = module.exports = function () {
+  // Middleware to create functions for adding pipes for query and response streams.
   this.documents(function (request, response, next) {
-    var pipes = [];
-    request.baucis.pipe = function (destination) {
-      if (destination !== undefined) return pipes.push(destination);
-      pipes.unshift(request.baucis.query.stream());
-      pipes.push(response);
-      var stream = es.pipeline.apply(es, pipes);
-      stream.on('error', next);
-    };
+    request.baucis.outgoing = pipeInterface(request.baucis.query.stream.bind(request.baucis.query), response);
+    request.baucis.incoming = pipeInterface(request, response);
     next();
   });
 };
