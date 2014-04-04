73877df9068d1c3427e051fa9f860195ddab88b1
William Riley-Land <william@kun.io>
2014-04-04 18:14:11 -0500

    Cleanup.

--- test/post.plural.js
+++ test/post.plural.js
@@ -9,7 +9,7 @@ describe('POST plural', function () {
   beforeEach(fixtures.vegetable.create);
   after(fixtures.vegetable.deinit);
 
-  it('should create a new object and return its ID (JSON)', function (done) {
+  it('should create a new object and return its ID', function (done) {
     var options = {
       url: 'http://localhost:8012/api/vegetables/',
       json: { name: 'Tomato' }
